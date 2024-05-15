3749527cf49116c7ea3c399c60d3b0bdb148629f
Priya Sandpa <21154@students.mrgs.school.nz>
2024-05-15 14:46:25 +1200

    Set width and height of window to be same as phone, made main_frame and changed the background colour of the main frame.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tkinter import *
 
+w_width= 360
+w_height= 675
+
 window=Tk()
-window.geometry("300x300")
+window.geometry(str(w_width) +"x"+ str(w_height))
 window.title("My App")
 
+main_frame = Frame(background="#F7EDE2", width=w_width, height=w_height)
+main_frame.pack()
+
+hello_label=Label(text="Hello World!")
 window.mainloop()
