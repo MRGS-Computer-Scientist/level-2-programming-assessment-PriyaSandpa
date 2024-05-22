fec823fc28f8363805ebc1c94b0b31bcd8c4cb23
Priya Sandpa <21154@students.mrgs.school.nz>
2024-05-22 14:48:31 +1200

    Made the buttons for the taskbar. Took time in positioning them. Realised that it doesn't position from center of the widget but positions horizontally from left of edge of widget and positions vertically from top edge.

--- main.py
+++ main.py
@@ -13,5 +13,13 @@ main_frame.pack()
 taskbar_frame=Frame(background="#F6BD60", width=w_width, height=50)
 taskbar_frame.pack()
 
-hello_label=Label(text="Hello World!")
+home_button=Button(taskbar_frame, text="Home", width=4, height=1)
+home_button.place(x=160, y=10)
+
+stats_button=Button(taskbar_frame, text="Stats", width=4, height=1)
+stats_button.place(x=60, y=10)
+
+settings_button=Button(taskbar_frame, text="Settings", width=4, height=1)
+settings_button.place(x=260, y=10)
+
 window.mainloop()
