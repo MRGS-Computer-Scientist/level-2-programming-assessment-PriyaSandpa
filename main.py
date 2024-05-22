360b5a67d13e113da1a33204c435a1e86fe86099
Priya Sandpa <21154@students.mrgs.school.nz>
2024-05-22 14:25:38 +1200

    Made a frame for the taskbar at the bottom, adjusted the size of the mainframe and the taskbar frame so that both fit. Still trying to understand python units for sizing

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ window=Tk()
 window.geometry(str(w_width) +"x"+ str(w_height))
 window.title("My App")
 
-main_frame = Frame(background="#F7EDE2", width=w_width, height=w_height)
+main_frame = Frame(background="#F7EDE2", width=w_width, height=625)
 main_frame.pack()
+
+taskbar_frame=Frame(background="#F6BD60", width=w_width, height=50)
+taskbar_frame.pack()
 
 hello_label=Label(text="Hello World!")
 window.mainloop()
