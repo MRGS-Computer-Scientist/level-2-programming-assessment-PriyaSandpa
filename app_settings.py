d58031405f8fa446e5467bab9fd656cfc8a39d51
Priya Sandpa <21154@students.mrgs.school.nz>
2024-06-01 19:51:44 +1200

    Moved the line "home_button=PhotoImage(file='Home.png')" from the appsettings file to the homescreen file. Named it home instead of home_button

--- app_settings.py
+++ app_settings.py
@@ -1,4 +1,3 @@
 from tkinter import*
 w_width= 360
 w_height= 675
-home_button=PhotoImage(file="Home.png")
--- home_screen.py
+++ home_screen.py
@@ -7,13 +7,15 @@ class App():
         self.window.geometry(str(w_width) +"x"+ str(w_height))
         self.window.title("My App")
 
+        home=PhotoImage(file="Home.png")
+
         self.main_frame = Frame(background="#F7EDE2", width=w_width, height=625)
         self.main_frame.pack()
 
         self.taskbar_frame=Frame(background="#F6BD60", width=w_width, height=50)
         self.taskbar_frame.pack()
 
-        self.home_button=Button(self.taskbar_frame, image=home_button, width=4, height=1)
+        self.home_button=Button(self.taskbar_frame, image=home, width=4, height=1)
         self.home_button.place(x=160, y=10)
 
         self.stats_button=Button(self.taskbar_frame, text="Stats", width=4, height=1,command=exit)
