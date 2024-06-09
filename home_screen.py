83a7e26c366a2d378f053ad04974721fccec5ba9
Priya Sandpa <21154@students.mrgs.school.nz>
2024-06-09 20:47:08 +1200

    Made the taskbar with the buttons working. Only error: when i click settings for the first time it opens the stats page, after that everything works fine.

--- home_screen.py
+++ home_screen.py
@@ -19,27 +19,105 @@ class App():
         image_help=Image.open("images/Help.png")
         help_icon=ImageTk.PhotoImage(image_help.resize((40,40)))      
 #--Main--------------------------------------------------------------------------
-        self.main_frame = Frame(background=main_bg_standard, width=w_width, height=625)
+        self.main_frame = Frame(background=main_bg_standard, 
+                                width=w_width, 
+                                height=675)
         self.main_frame.pack()
         self.main_frame.pack_propagate(False)
+        self.home_label=Label(self.main_frame,
+                              text="Home", 
+                              font="Verdana 30", 
+                              background=main_bg_standard)
+        self.home_label.pack()
+        
+        self.stats_frame = Frame(background=main_bg_standard, 
+                                 width=w_width, 
+                                 height=675)
+        self.stats_frame.pack()
+        self.stats_frame.pack_propagate(False)
+        self.stats_label=Label(self.stats_frame,
+                               text="Stats", 
+                               font="Verdana 30", 
+                               background=main_bg_standard)
+        self.stats_label.pack()
+        
+        self.settings_frame = Frame(background=main_bg_standard, 
+                                    width=w_width, 
+                                    height=675)
+        self.settings_frame.pack()
+        self.settings_frame.pack_propagate(False)
+        self.settings_label=Label(self.settings_frame,
+                                  text="Settings", 
+                                  font="Verdana 30", 
+                                  background=main_bg_standard)
+        self.settings_label.pack()
 #--Taskbar-----------------------------------------------------------------------
-        self.taskbar_frame=Frame(background=taskbar_bg_standard, width=w_width, height=50)
-        self.taskbar_frame.pack()
-        self.taskbar_frame.pack_propagate(False)
+        self.taskbar_frame=Frame(background=taskbar_bg_standard, 
+                                 width=w_width, 
+                                 height=50)
+        self.taskbar_frame.place(x=0, y=w_height-50)
 
-        self.home_button=Button(self.taskbar_frame, image=home_icon, background=taskbar_bg_standard, width=30, height=30, highlightthickness=0, bd=0, activebackground=taskbar_bg_standard)
+        self.home_button=Button(self.taskbar_frame,
+                                image=home_icon, 
+                                background=taskbar_bg_standard, 
+                                width=30, 
+                                height=30, 
+                                highlightthickness=0, 
+                                bd=0, 
+                                activebackground=taskbar_bg_standard, 
+                                command=lambda: self.go_to_frame("Home"))
         self.home_button.place(x=165, y=10)
 
-        self.stats_button=Button(self.taskbar_frame, image=stats_icon, background=taskbar_bg_standard, width=30, height=30, highlightthickness=0, bd=0, command=exit, activebackground=taskbar_bg_standard)
+        self.stats_button=Button(self.taskbar_frame, 
+                                 image=stats_icon, 
+                                 background=taskbar_bg_standard, 
+                                 width=30, height=30, 
+                                 highlightthickness=0, 
+                                 bd=0, 
+                                 command=lambda: self.go_to_frame("Stats"), 
+                                 activebackground=taskbar_bg_standard)
         self.stats_button.place(x=55, y=10)
 
-        self.settings_button=Button(self.taskbar_frame, image=settings_icon, background=taskbar_bg_standard, width=30, height=30, highlightthickness=0, bd=0, command=exit, activebackground=taskbar_bg_standard)
+        self.settings_button=Button(self.taskbar_frame, 
+                                    image=settings_icon, 
+                                    background=taskbar_bg_standard, 
+                                    width=30, 
+                                    height=30, 
+                                    highlightthickness=0, 
+                                    bd=0, 
+                                    command=lambda: self.go_to_frame("Settings"), 
+                                    activebackground=taskbar_bg_standard)
         self.settings_button.place(x=275, y=10)
 #--Help Button-------------------------------------------------------------------
-        self.help_button=Button(self.main_frame, image=help_icon, background=main_bg_standard, width=40, height=40, highlightthickness=0, bd=0, border=0, activebackground=main_bg_standard)
+        self.help_button=Button(self.main_frame, 
+                                image=help_icon, 
+                                background=main_bg_standard, 
+                                width=40, height=40, 
+                                highlightthickness=0, 
+                                bd=0, border=0, 
+                                activebackground=main_bg_standard)
         self.help_button.place(x=310, y=15)
 #--Mainloop----------------------------------------------------------------------
         self.window.mainloop()
+    
+    def go_to_frame(self, next_frame):
+
+        if self.current_frame == "Home":
+                self.main_frame.pack_forget()
+        elif self.current_frame == "Stats":
+                self.stats_frame.pack_forget()
+        elif self.current_frame == "Settings":
+                self.settings_frame.pack_forget()
+        
+        if next_frame == "Stats":
+                self.stats_frame.pack()
+                self.current_frame = "Stats"
+        elif next_frame == "Settings":
+                self.settings_frame.pack()
+                self.current_frame = "Settings"
+        elif next_frame == "Home":
+                self.main_frame.pack()
+                self.current_frame = "Home"
 
 if __name__=="__main__":
     app=App()
